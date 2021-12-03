75cfee20fa9063a952153c6dddd93391aa36b3fb
Uday Yadav <dev117uday@gmail.com>
2021-12-03 17:41:56 +0530

    init JPA

---
+++ jpa/src/main/java/com/example/jpa/entity/Guardian.java
@@ -0,0 +1,27 @@
+package com.example.jpa.entity;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@AttributeOverrides({
+		@AttributeOverride(name = "name", column = @Column(name = "guardian_name")),
+		@AttributeOverride(name = "email", column = @Column(name = "guardian_email")),
+		@AttributeOverride(name = "mobile", column = @Column(name="guardian_mobile"))
+})
+@Builder
+public class Guardian {
+	private String gaurdianName;
+	private String guardingEmail;
+	private String guardianMobile;
+}
---
+++ jpa/src/main/java/com/example/jpa/repository/StudentRepository.java
@@ -0,0 +1,44 @@
+package com.example.jpa.repository;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.example.jpa.entity.Student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+	List<Student> findByFirstName(String firstName);
+
+	// JPQL-native
+	@Query(value = "select * from tbl_student s where s.email_address = ?1", nativeQuery = true)
+	Student getStudentByEmailAddressNative(String emailId);
+
+	// JPQL
+	@Query("select s from Student s where s.emailId = ?1")
+	Student findStudentByEmail(String emailId);
+
+	// Native Named Param
+	@Query (
+		value = "select * from tbl_student s where s.email_address = :emailId",
+		nativeQuery = true
+	)
+	Student getStudentByEmailAddressNativeNamed(@Param("emailId") String emailId);
+
+	@Modifying
+	@Transactional
+	@Query (
+		value = "update tbl_student set first_name = ?1 where email_address = ?2",
+		nativeQuery = true
+	)
+	Integer updatStudentEmailIdByFirstName(String firstName, String emailId);
+
+
+}
