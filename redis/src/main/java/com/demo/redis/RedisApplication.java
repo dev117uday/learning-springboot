73b671a48e2bc8acba3240b5fb26e697543c7adb
Uday Yadav <dev117uday@gmail.com>
2021-12-23 23:10:30 +0530

    added redis demo

---
+++ redis/src/main/java/com/demo/redis/RedisApplication.java
@@ -0,0 +1,56 @@
+package com.demo.redis;
+
+import java.util.List;
+
+import com.demo.redis.entity.Product;
+import com.demo.redis.repository.ProductDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/product")
+public class RedisApplication {
+
+	@Autowired
+	private ProductDao productDao;
+
+	@GetMapping
+	public Product getSample() {
+		return new Product();
+	}
+
+	@PostMapping
+	public Product saveProduct(@RequestBody Product product) {
+		return productDao.saveProduct(product);
+	}
+
+	@GetMapping("/all")
+	public List<Object> getAllProducts() {
+		return productDao.findAllProducts();
+	}
+
+	@GetMapping("/{id}")
+	public Object findProduct(@PathVariable int id) {
+		return productDao.findProductById(id);
+	}
+
+	@DeleteMapping("/{id}")
+	public Boolean removeProduct(@PathVariable int id) {
+		return productDao.deleteProduct(id);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplication.class, args);
+	}
+
+}
---
+++ redis/src/main/java/com/demo/redis/entity/Product.java
@@ -0,0 +1,20 @@
+package com.demo.redis.entity;
+
+import java.io.Serializable;
+
+import org.springframework.data.redis.core.RedisHash;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash("Product")
+public class Product implements Serializable {
+	private int id;
+	private String name;
+	private int qty;
+	private long price;
+}
