4fff96bb41e5ce0b833b55cd4837827e53283924
Uday Yadav <yadav117uday@gmail.com>
2022-03-17 16:36:31 +0530

    config oauth user details

---
+++ spring-security/oauth-authorization-server/src/main/java/app/udayyadav/oauthserver/entity/User.java
@@ -0,0 +1,28 @@
+package app.udayyadav.oauthserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String userEmail;
+    @Column(length = 60)
+    private String password;
+    private String role;
+    private boolean isEnabled = false;
+}
---
+++ spring-security/oauth-authorization-server/src/main/java/app/udayyadav/oauthserver/service/CustomUserDetailsService.java
@@ -0,0 +1,52 @@
+package app.udayyadav.oauthserver.service;
+
+import app.udayyadav.oauthserver.entity.User;
+import app.udayyadav.oauthserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUserEmail(userEmail);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUserEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                true, true, true, getAuthorities(List.of(user.getRole()))
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(List<String> roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (String role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+        return authorities;
+    }
+}
