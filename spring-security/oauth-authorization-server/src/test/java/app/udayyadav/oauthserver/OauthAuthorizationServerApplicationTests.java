b48ffe1a5ed8ceaf813a5717b5fe63d95336771b
Uday Yadav <yadav117uday@gmail.com>
2022-03-17 16:18:44 +0530

    init oauth server

---
+++ spring-security/oauth-authorization-server/src/test/java/app/udayyadav/oauthserver/OauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.udayyadav.oauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
