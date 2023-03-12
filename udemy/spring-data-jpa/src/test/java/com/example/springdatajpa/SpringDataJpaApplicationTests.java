e82bb5fda6dd28c1b9d41c79f5350f456f8d8024
Uday Yadav <yadav117uday@gmail.com>
2023-03-12 20:10:21 +0530

    backup

--- udemy/spring-data-jpa/src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
+++ udemy/spring-data-jpa/src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.springdatajpa;
 
+import com.example.springdatajpa.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
 
+	private final BookRepository bookRepository;
+
+	@Autowired
+	public SpringDataJpaApplicationTests(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testBookRepo() {
+		long count = bookRepository.count();
+		assertThat(count).isGreaterThan(0);
 	}
 
 }
